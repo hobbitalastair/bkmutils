9f1092ce15d0f906dbb2c823755268aa2ca046f7
Alastair Hughes <hobbitalastair@gmail.com>
2021-01-17 09:09:56 +1300

    bkm-open: support opening multiple bookmarks at once

--- bkm-open.sh
+++ bkm-open.sh
@@ -3,26 +3,28 @@
 # Open a bookmark file
 #
 
-if [ "$#" -ne 1 ]; then
-    printf "usage: %s file\n" "$0"
+if [ "$#" -eq 0 ]; then
+    printf "usage: %s file [file ...]\n" "$0"
     exit 1
 fi
 
-if [ ! -r "$1" ]; then
-    printf "%s: not a file: %s\n" "$0" "$1"
-    exit 2
-fi
+for file in "$@"; do
+    if [ ! -r "${file}" ]; then
+        printf "%s: not a file: %s\n" "$0" "${file}"
+        continue
+    fi
 
-uri="$(sed -n -e '/^uri=/s/^uri=//p' < "$1")"
+    uri="$(sed -n -e '/^uri=/s/^uri=//p' < "${file}")"
 
-if [ -z "${uri}" ]; then
-   printf '%s: no uri in bookmark file, or malformed file\n' "$0"
-   exit 3
-fi
+    if [ -z "${uri}" ]; then
+        printf '%s: no uri in bookmark file, or malformed file\n' "$0"
+        continue
+    fi
 
-if [ "$(printf "${uri}" | wc -l)" -gt 0 ]; then
-    printf '%s: multiple uri in bookmark file\n' "$0"
-    exit 3
-fi
+    if [ "$(printf "${uri}" | wc -l)" -gt 0 ]; then
+        printf '%s: multiple uri in bookmark file\n' "$0"
+        continue
+    fi
 
-exec xdg-open "${uri}"
+    xdg-open "${uri}"
+done
